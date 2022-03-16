e1ec3a641fc93bfbae1d1bb6c64735d8d41c2801
ama22ni <astroamanii@gmail.com>
2022-03-16 03:50:05 +0300

    add orintiation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'Screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
   await Firebase.initializeApp();
   runApp(MyApp());
 }
@@ -15,16 +16,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'حًسبة',
-      
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home:  Directionality( // add this
-        textDirection: TextDirection.rtl, // set this property 
+      home: Directionality(
+        // add this
+        textDirection: TextDirection.rtl, // set this property
         child: LoginScreen(),
       ),
     );
   }
-  
 }
